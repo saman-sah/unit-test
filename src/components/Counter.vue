a8de2f0468f7c78b7d3b8a5d918e41c1ab21b7f9
SamanSah <sahraei.saman91@gmail.com>
2023-08-01 05:57:35 +0330

    unit test $emit

---
+++ src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="increament">clicks: {{ counter }}</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increament() {
+            this.counter++;
+            this.$emit("count", this.counter, "Hello")
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tests/unit/Counter.spec.js
@@ -0,0 +1,18 @@
+import { mount } from "@vue/test-utils"
+import Counter from "@/components/Counter.vue"
+
+describe("Counter.vue", ()=> {
+    it("testing emit counter", async()=> {
+        const wrapper= mount(Counter)
+        await wrapper.find('button').trigger('click')
+        await wrapper.find('button').trigger('click')
+        console.log(wrapper.emitted());
+
+        // First click
+        expect(wrapper.emitted().count[0][0]).toBe(1)
+        expect(wrapper.emitted().count[0][1]).toBe("Hello")
+        // Second click
+        expect(wrapper.emitted().count[1][0]).toBe(2)
+        expect(wrapper.emitted().count[1][1]).toBe("Hello")
+    })
+})
